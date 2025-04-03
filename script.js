1b3cd35b3720e70ed4431d568736e44442533abe
divyanshi220106 <163905472+divyanshi220106@users.noreply.github.com>
2025-04-03 14:35:33 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+// function sayHello(){
+//     let you=prompt("What's your name?");
+//     console.log("Hello",you + " ! ");
+// }
+// sayHello();
+// let varContainingFunction=function(){
+//     let varInFunction="I 'm in a function .";
+//     console.log("hi there!", varInFunction);
+// };
+// varContainingFunction();
+function tester(para1,para2){
+    return para1 + " "+ para2;
+}
+const arg1="argument 1";
+const arg2="Argument 2";
+tester(arg1,arg2);
+console.log(arg1,arg2);
